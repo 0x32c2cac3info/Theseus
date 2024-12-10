0a4b984565a1205f3987e19518b810a6051e4cf7
0x32c2cac3info <0x32c2cac3info@users.noreply.github.com>
2024-12-10 01:52:31 +0000

    Makefile: add support for building on FreeBSD (#1103) 1fbfe567075a65ed749b6680db1aeb538819c70c

--- doc/captain/sidebar-items.js
+++ doc/captain/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["init","Initialize the Captain, which is the main module that steers the ship of Theseus. "]]};
+window.SIDEBAR_ITEMS = {"fn":["init"],"struct":["DropAfterInit","MulticoreBringupInfo"]};
--- doc/network_interface_card/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"trait":[["NetworkInterfaceCard","A trait that defines the necessary minimum functions that all network interface card (NIC) drivers should implement. "]]};
